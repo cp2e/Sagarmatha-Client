bc41b44929304596c7286e1d540e680b7002697e
eilamhome <eilamcohen2@gmail.com>
2019-01-16 01:19:34 +0200

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AnalysisComponent } from './analysis/analysis.component';
 
-const routes: Routes = [];
+const routes: Routes =  [
+
+  {path: '',component: CustomerComponent},
+  {path: 'Customer',component: CustomerComponent},
+  {path: 'Orders',component: OrdersComponent},
+  {path: 'Analysis',component: AnalysisComponent},
+ 
+  
+  
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CustomerComponent } from './customer/customer.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CustomerComponent,
+    PaginationComponent,
+    OrdersComponent,
+    AnalysisComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
